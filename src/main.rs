e3cb9943b5ead0ea8ac47bd6222b175e9c493740
jesopo <github@lolnerd.net>
2023-01-09 22:55:56 +0000

    pedanticise clippy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![deny(clippy::pedantic)]
+#![deny(clippy::dbg_macro)]
+#![deny(clippy::debug_assert_with_mut_call)]
+#![deny(clippy::equatable_if_let)]
+#![deny(clippy::if_then_some_else_none)]
+#![deny(clippy::same_name_method)]
+#![deny(clippy::try_err)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+
 mod config;
 mod error;
 mod filters;
